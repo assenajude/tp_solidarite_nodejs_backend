aec8fef08c8d11561dd04cdd7063eeaf85d58a4f
albesty <albesty01@gmail.com>
2021-02-13 09:39:48 +0000

    modify migrations files again

--- db/migrations/20201103165030-create-plan.js
+++ db/migrations/20201103165030-create-plan.js
@@ -9,7 +9,7 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       libelle: {
-        type: SequelizePSTRING
+        type: Sequelize.STRING
       },
       descripPlan: {
         type: Sequelize.STRING
