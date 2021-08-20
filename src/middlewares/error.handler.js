adc8a9b884b86ea732316a1b28ae6bf01685692e
assenajude@gmail.com <assenajude@gmail.com>
2021-08-20 13:34:18 +0000

    update on 20/08/2021 13:34

--- src/middlewares/error.handler.js
+++ src/middlewares/error.handler.js
@@ -2,6 +2,6 @@ const logger = require('../startup/logger')
 module.exports = async (error, req, res, next) => {
     if (!error.statusCode) return  error.statusCode = 500;
     logger.error(error.message);
-    return res.status(error.statusCode).send({'error': error.message});
+    return res.status(error.statusCode).send({'error': error.message?error.message : error});
 
 }
